aca6051cf9527e3185689b17ae1930eb10b0368a
BrocatoSantino <arkadain405@gmai.com>
2026-08-11 23:58:24 -0300

    Fix de diseño: login centrado, flechas seguras, nuevo logo SVG, nombre actualizado y duración real de 45m

--- app/config.py
+++ app/config.py
@@ -8,10 +8,10 @@ WA_ACCESS_TOKEN = os.getenv('WA_ACCESS_TOKEN')
 WA_VERIFY_TOKEN = os.getenv('WA_VERIFY_TOKEN')
 WA_APP_SECRET = os.getenv('WA_APP_SECRET')
 
-BUSINESS_NAME = os.getenv('BUSINESS_NAME', 'puerto.barberr')
+BUSINESS_NAME = os.getenv('BUSINESS_NAME', 'puerto barbershop')
 BUSINESS_SHIFTS = [
     {"start": "09:30", "end": "12:30"},
-    {"start": "16:00", "end": "20:30"}
+    {"start": "16:00", "end": "21:15"}
 ]
 SLOT_DURATION_MINUTES = int(os.getenv('SLOT_DURATION_MINUTES', 45))
 BUSINESS_DAYS = [0, 1, 2, 3, 4, 5]  # Lunes a Sábado
